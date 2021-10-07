4ed1e030e44011f042b194c9d0e173bfd0a85073
Andrew den Hertog <andrew.denhertog@gmail.com>
2021-10-08 07:33:37 +1000

    initial commit

---
+++ src/bus-redis-symbols.ts
@@ -0,0 +1,7 @@
+export const BUS_REDIS_SYMBOLS = {
+  TransportConfiguration: Symbol.for('node-ts/bus-redis/transport-configuration')
+}
+
+export const BUS_REDIS_INTERNAL_SYMBOLS = {
+  RedisTransport: Symbol.for('node-ts/bus-redis/redis-transport')
+}
